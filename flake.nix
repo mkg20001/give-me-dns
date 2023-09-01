7630f7ce9ddb52bc8892655a163bef2234418d6a
Maciej Krüger <mkg20001@gmail.com>
2023-09-02 01:48:04 +0200

    feat: module

--- flake.nix
+++ flake.nix
@@ -1,24 +1,35 @@
 {
-  description = "Tiny script to help you test nixpkgs changes";
+  description = "Temporary DNS names for IPv6 addresses";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
 
     let
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
 
     {
-      overlay = final: prev: {
-        lxd-nixpkgs-test = prev.callPackage ./. {};
+      overlays.default = final: prev: {
+        give-me-dns = prev.callPackage ./. {};
       };
 
-      defaultPackage = forAllSystems (system: (import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      }).lxd-nixpkgs-test);
+      packages = forAllSystems (system:
+        let
+          pkgs = (import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          });
+        in
+          {
+            inherit (pkgs) give-me-dns;
 
+            default = pkgs.give-me-dns;
+          });
+
+      nixosModules = {
+        give-me-dns = import ./module.nix;
+      };
     };
 }
---
+++ module.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.services.give-me-dns;
+  format = pkgs.formats.yaml {};
+in
+{
+  options = {
+    services.give-me-dns = {
+      enable = mkEnableOption "give-me-dns";
+
+      settings = mkOption {
+        desc = "Settings for give-me-dns";
+        type = formnat.type;
+      };
+    };
+  };
+  config = lib.mkIf (cfg.enable) {
+    services.give-me-dns = {
+      settings = mapAttrs' (name: value: nameValuePair (name) (mkDefault value)) {
+        domain = "6dns.me";
+        ttl = "48h";
+        idlen = 3;
+        dns_addr = "::";
+        dns_port = 53;
+        net_addr = "::";
+        net_port = 9999;
+        store_file = "/var/lib/give-me-dns/db";
+        dns_ns = "ns1.give-me-dns.net";
+        dns_mname = "mkg20001.gmail.com.";
+      };
+    };
+
+    systemd.services.give-me-dns = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        ExecStart = "${pkgs.give-me-dns}/bin/give-me-dns ${format.generate "config.yaml" cfg}";
+        StateDirectory = "give-me-dns";
+      };
+    };
+
+    networking.firewall.allowedUDPPorts = [
+      cfg.settings.dns_port
+    ];
+
+    networking.firewall.allowedTCPPorts = [
+      cfg.settings.net_port
+    ];
+  };
+}

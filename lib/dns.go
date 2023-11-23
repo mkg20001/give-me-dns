3420b92f8e0020f0c7b0eae7628c88d2cbaa95ab
Maciej Krüger <mkg20001@gmail.com>
2023-11-23 21:44:01 +0100

    misc: cleanup

--- lib/dns.go
+++ lib/dns.go
@@ -70,34 +70,6 @@ func parseDNSQuery(r *dns.Msg, m *dns.Msg, store *Store, s *DNSSECSigner) {
 		soa.Serial = store.GetSerial()
 		soa.Expire = 1
 
-		var ip net.IP
-
-		log.Printf("Query for %s\n", q.Name)
-		labelIndexes := dns.Split(q.Name)
-		if len(labelIndexes) < 2 {
-			return
-		}
-		lastBlock := strings.ToLower(q.Name)[labelIndexes[0] : labelIndexes[1]-1]
-		ip, err := store.ResolveEntry(lastBlock)
-		if err != nil {
-			sentry.CaptureException(err)
-			log.Printf("Failed to resolve: %s", err)
-			return
-		}
-		if ip != nil {
-			r := new(dns.AAAA)
-			r.Hdr = dns.RR_Header{
-				Name:   q.Name,
-				Rrtype: dns.TypeAAAA,
-				Class:  dns.ClassINET,
-				Ttl:    uint32(store.Config.TTL.Seconds()),
-			}
-			r.AAAA = ip
-
-			log.Printf("Query for %s - Resolved %s\n", q.Name, ip)
-			m.Answer = append(m.Answer, r)
-		}
-
 		switch q.Qtype {
 		case dns.TypeDNSKEY:
 			if ismain {
@@ -125,7 +97,7 @@ func parseDNSQuery(r *dns.Msg, m *dns.Msg, store *Store, s *DNSSECSigner) {
 				m.Answer = append(m.Answer, soa)
 			}
 		case dns.TypeAAAA:
-			ip = resolveDomain(q, store)
+			ip := resolveDomain(q, store)
 			if ip != nil {
 				r := new(dns.AAAA)
 				r.Hdr = dns.RR_Header{
@@ -183,7 +155,7 @@ func parseDNSQuery(r *dns.Msg, m *dns.Msg, store *Store, s *DNSSECSigner) {
 				}
 				m.Ns = append(m.Ns, rrsig2)
 			} else {
-				ip = resolveDomain(q, store)
+				ip := resolveDomain(q, store)
 				if !ismain && ip == nil {
 					m.Rcode = dns.RcodeNameError
 				}
